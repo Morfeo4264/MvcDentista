d5b1ed24c885eac15cc084886f8f2623e611cf97
Morfeo4264 <armestonahuel2@hotmail.com>
2022-05-17 23:36:52 -0300

    Agregar archivos de proyecto.

---
+++ Models/Paciente.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MvcDentista.Models
+{
+    public class Paciente
+
+    {
+
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        [DataType(DataType.Date)]
+
+        public DateTime FechaAsistencia { get; set; }
+
+        public string Domicilio { get; set; }
+
+        public string Localidad { get; set; }
+
+        public string Profesion { get; set; }
+
+        public int Edad { get; set; }
+
+
+
+    }
+
+}
+
